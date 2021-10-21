ea65176c92c9c81dc666393d6a3f7546d259856c
Venkatesh Ramoju <rnbvenkatesh@hotmail.com>
2021-10-20 23:49:51 -0700

    Initial Commit

---
+++ hspmanagement/src/main/java/com/rnbv/project/hspmanagement/HspmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.rnbv.project.hspmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HspmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HspmanagementApplication.class, args);
+	}
+
+}
---
+++ hspmanagement/src/test/java/com/rnbv/project/hspmanagement/HspmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rnbv.project.hspmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HspmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
